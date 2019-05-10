647ee59d7d27ebf374b612131a770c5915b02b23
JoseUrdiales <jose-urdiales@hotmail.com>
2019-05-10 10:28:53 -0500

    Clase 10-05-2019

---
+++ CiclosRepetitivos/src/ciclowhile/CicloTres.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ciclowhile;
+
+/**
+ *
+ * @author Jose Luis
+ */
+public class CicloTres {
+    public static void main(String[] args) {
+        int contador = 1;
+        int limite = 5;
+        int suma = 0;
+        System.out.printf("%s", "La suma de:\n");
+        while (contador <= limite){
+            suma = suma + contador;
+            System.out.printf("%d\n", contador);
+            contador = contador + 1;
+        }
+        System.out.printf("Tiene un resultado de: %d\n", suma);
+    }
+}
